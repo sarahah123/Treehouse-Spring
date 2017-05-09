4376b3d2507a930cbfaeaaab7cc6e15692779612
sarahah123 <shansber55@gmail.com>
2017-05-08 20:43:47 -0400

    Add giflab and test

---
+++ Testing IntelliJ/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+/**
+ * Created by Sarah on 5/8/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for(int x = 0; x < 10; x++) {
+            System.out.println("Testing...");
+        }
+        System.out.println("Done!");
+    }
+}
